e2b9e9925e0c5c924d67dbf17f08c098c16e9be2
rag-nar1 <ragnar1242003@gmail.com>
2024-12-23 18:40:53 +0200

    Auth request impelmented

--- go.mod
+++ go.mod
@@ -8,8 +8,10 @@ require (
 	github.com/apache/arrow-go/v18 v18.0.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/google/flatbuffers v24.3.25+incompatible // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/marcboeker/go-duckdb v1.8.3 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
--- server.go
+++ server.go
@@ -6,35 +6,38 @@ import (
 	"log"
 	"net"
 	"os"
-
+	"bufio"
+	"strings"
+	"github.com/joho/godotenv"
 	_ "github.com/marcboeker/go-duckdb"
+	_ "github.com/lib/pq"
 )
 
-
-const (
-	dbscheme string = `
-		CREATE TABLE USER (
-			id INT PRIMARY KEY,
-			username VARCHAR UNIQUE
-		);
-		CREATE TABLE DB (
-			id INT PRIMARY KEY,
-			dbname VARCHAR,
-			userid INT REFERENCES USER(id)
-		);
-		CREATE UNIQUE INDEX USER_INDEX ON USER (username);
-		CREATE UNIQUE INDEX DB_INDEX ON DB (userid , dbname);
-	`
-	dbname string = "db.duckdb"
-)
 
 var (
 	currentdir string
 	dbpath string
 	db *sql.DB
+	connectionString string = "postgres://%s:%s@%s:%s/%s?sslmode=disable" // user , password , hostname , port , database
+	PostgresHost string
+	PostgresPort string
+	PostgresUser string
+	PostgresPass string
+	PostgresDbName string
+	ServerHost = "localhost"
+	ServerPort = "2003"
 )
 
-
+func checkFetal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func checkLog(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
 
 
 /**
@@ -50,11 +53,92 @@ func CreateListener(address string) (net.Listener , error) {
 	return listner , nil
 }
 
+/**
+ * function handles connections while it's open and respones to requets
+ * requests format
+	* AUTH < Username > < Password > < Database >
+	* QUERY < SqlQuery >
+*/
 
-func HandleConnection(connection net.Conn) error {
+
+func HandleConnection(connection net.Conn) {
 	defer connection.Close()
 
-	return nil
+	
+	
+	reader := bufio.NewReader(connection)
+	authenticated := false
+	var Username , Password , Database string
+	var UID , DBID int
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil { // send error
+			connection.Write([]byte("Error while reading from the connection\nConnection Closed"))
+			return 
+		}
+		
+		message = strings.TrimSpace(message)
+		request := strings.Split(message , " ")
+
+		if request[0] == "AUTH" {
+			if len(request) != 4 {
+				connection.Write([]byte("Error invalid request\n"))
+				continue
+			}
+
+			Username , Password , Database = request[1] , request[2] , request[3]
+			userAuth , err := db.Query(fmt.Sprintf("SELECT id , password FROM users WHERE username LIKE '%s'" , Username))
+			if err != nil {
+				connection.Write([]byte("Error while executing query user\n" + err.Error()))
+				continue
+			}
+			fmt.Println(userAuth)
+			var correctPass *string = new(string)
+			var uid *int = new(int)
+			userAuth.Next()
+			err = userAuth.Scan(uid , correctPass)
+			if err != nil {
+				connection.Write([]byte("Error invalid Username\n" + err.Error()))
+				continue
+			}
+
+			
+			//check the password
+			if *correctPass != Password {
+				connection.Write([]byte("Error wrong password\n"))
+				continue
+			}
+
+			UID = *uid
+
+			dbAuth , err := db.Query(fmt.Sprintf("SELECT id FROM databases WHERE name LIKE '%s' AND userid = %d" , Database , UID))
+			if err != nil {
+				connection.Write([]byte("Error while executing query db\n" + err.Error()))
+				continue
+			}
+
+			var dbid *int = new(int)
+			dbAuth.Next()
+			err = dbAuth.Scan(dbid)
+			if err != nil {
+				connection.Write([]byte("Error this user does not own this database \n" + err.Error()))
+				continue
+			}
+
+			DBID = *dbid
+			authenticated = true
+
+			connection.Write([]byte("success\n"))
+			fmt.Println(UID , DBID , authenticated)
+			continue
+		}
+
+
+		
+
+
+	}
+
 }
 
 /**
@@ -75,25 +159,26 @@ func Start(listner net.Listener) error {
 }
 
 
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 
 func init() {
-	currentdir , _ = os.Getwd()
-	dbpath = currentdir + "/DB/" + dbname
-	var err error
-	db , err = sql.Open("duckdb" , dbpath)
-	check(err)
+	err := godotenv.Load()
+	PostgresHost = os.Getenv("PostgresHost")
+	PostgresPort = os.Getenv("PostgresPort")
+	PostgresUser = os.Getenv("PostgresUser")
+	PostgresPass = os.Getenv("PostgresPass")
+	PostgresDbName = os.Getenv("PostgresDbName")
+	checkFetal(err)
+	connection := fmt.Sprintf(connectionString , PostgresUser , PostgresPass , PostgresHost , PostgresPort , PostgresDbName)
+ 	db , err = sql.Open("postgres" , connection)
+	checkFetal(err)
+	fmt.Println("connected to postgresql")
 }
 
 func main() {
 	defer db.Close()
-	_ , err := db.Exec(dbscheme)
-	check(err)
-	fmt.Println("created db")
-
+	listener , err := CreateListener(ServerHost + ":" + ServerPort)
+	checkFetal(err)
+	err = Start(listener)
+	checkLog(err)
 }
