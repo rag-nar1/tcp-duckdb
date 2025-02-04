d9b64c3820f4815435a2d4ed6d72d959707d4a33
rag-nar1 <ragnar1242003@gmail.com>
2025-02-04 20:08:04 +0200

    added listner and the main server loop

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,36 +1,92 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net"
 	"os"
-	"database/sql"
+	"strings"
 
+	"github.com/joho/godotenv"
 	_ "github.com/marcboeker/go-duckdb"
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/joho/godotenv"
 )
 
 type Server struct {
-	// db connection bools
-	Duckdb *sql.DB  
+	// db connection bool
 	Sqlitedb *sql.DB
+	Port string
+	Address string
 }
 
+func NewServer() (*Server , error){
+	dbconn , err := sql.Open("sqlite3",os.Getenv("DBdir"))
+	if err != nil {
+		return nil , err
+	}
+	
+	server := &Server{
+		Sqlitedb: dbconn,
+		Port: os.Getenv("ServerPort"),
+	}
+	server.Address = os.Getenv("ServerAddr") + ":" + server.Port
+	return server , nil
+}
 
 func init() {
 	err := godotenv.Load() // load environement variables
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+}
+
+func HandleConnection(conn *net.Conn) {
+	log.Println("Serving " + (*conn).RemoteAddr().String())
+
+	route := make([]byte, 1024)
+	n , err := (*conn).Read(route)
+	if err != nil {
+		log.Println("ERROR" , err)
+		return
+	}
+
+	request := strings.Split(string(route[0 : n]) , " ")
+	var UserName , password string
+	var UID int
+	if request[0] == "Login" {
+		// Todo: impelement authantication handler
+		
+	} else {
+		log.Println("ERROR: BAD request")
+	}
+
 }
 
 
 func main() {
 
+	server , err := NewServer()
+	if err != nil{
+		log.Fatal(err)
+	}
 
+	listener , err := net.Listen("tcp", server.Address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
 
+	log.Println("listening to " + server.Address)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go HandleConnection(&conn)
+
+		defer conn.Close()
+	}
 
 
 }
