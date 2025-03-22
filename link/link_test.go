4159b42a8c61b5059dff8f0e9ea47de1a70e3a35
rag-nar1 <ragnar1242003@gmail.com>
2025-03-22 21:40:23 +0200

    added comments to tests

--- link/link_test.go
+++ link/link_test.go
@@ -11,6 +11,7 @@ import (
 	_ "github.com/marcboeker/go-duckdb"
 )
 
+// TestBasicLink tests the basic linking of a database
 func TestBasicLink(t *testing.T) {
 	dbname := "db2"
 	connStr := "postgresql://postgres:1242003@localhost:5432/testdb"
@@ -19,21 +20,22 @@ func TestBasicLink(t *testing.T) {
 	conn := utils.StartUp()
 	defer conn.Close()
 	err := utils.LoginAsAdmin(conn)
-	assert.Nil(t, err) // Ensure no error during startup
+	assert.Nil(t, err) // Ensure no error during login as admin
 
 	err = utils.CreateDB(conn, dbname)
-	assert.Nil(t, err) // Ensure no error during creating db
+	assert.Nil(t, err) // Ensure no error during database creation
 
 	err = utils.Link(conn, dbname, connStr)
-	assert.Nil(t, err) // Ensure no error during linking db
+	assert.Nil(t, err) // Ensure no error during linking database
 
-	err = utils.ConnectDb(conn, dbname);
-	assert.Nil(t, err)
+	err = utils.ConnectDb(conn, dbname)
+	assert.Nil(t, err) // Ensure no error during connecting to the database
 
 	err = utils.Query(conn, "select * from t1;")
-	assert.Nil(t, err) // Ensure no error during linking db
+	assert.Nil(t, err) // Ensure no error during querying the database
 }
 
+// TestDoubleLink tests linking the same database to two different connections
 func TestDoubleLink(t *testing.T) {
 	dbname := "db2"
 	connStr := "postgresql://postgres:1242003@localhost:5432/testdb"
@@ -43,19 +45,20 @@ func TestDoubleLink(t *testing.T) {
 	conn := utils.StartUp()
 	defer conn.Close()
 	err := utils.LoginAsAdmin(conn)
-	assert.Nil(t, err) // Ensure no error during startup
+	assert.Nil(t, err) // Ensure no error during login as admin
 
 	err = utils.CreateDB(conn, dbname)
-	assert.Nil(t, err) // Ensure no error during creating db
+	assert.Nil(t, err) // Ensure no error during database creation
 
 	err = utils.Link(conn, dbname, connStr)
-	assert.Nil(t, err) // Ensure no error during linking db
+	assert.Nil(t, err) // Ensure no error during linking first connection
 
 	err = utils.Link(conn, dbname, connStr2)
 	log.Println(err)
-	assert.NotNil(t, err) // Ensure no error during linking db
+	assert.NotNil(t, err) // Ensure error during linking second connection
 }
 
+// TestNormLink tests linking a database with a normal user
 func TestNormLink(t *testing.T) {
 	dbname := "db2"
 	connStr := "postgresql://postgres:1242003@localhost:5432/testdb"
@@ -64,22 +67,24 @@ func TestNormLink(t *testing.T) {
 	conn := utils.StartUp()
 	defer conn.Close()
 	err := utils.LoginAsAdmin(conn)
-	assert.Nil(t, err) // Ensure no error during startup
+	assert.Nil(t, err) // Ensure no error during login as admin
 
 	err = utils.CreateDB(conn, dbname)
-	assert.Nil(t, err) // Ensure no error during creating db
+	assert.Nil(t, err) // Ensure no error during database creation
 
 	err = utils.CreateUser(conn, "ragnar", "ragnar")
-	assert.Nil(t, err)
+	assert.Nil(t, err) // Ensure no error during user creation
+
 	conn = utils.StartUp()
 	defer conn.Close()
-	err = utils.Login(conn,"ragnar", "ragnar")
-	assert.Nil(t, err) // Ensure no error during startup
+	err = utils.Login(conn, "ragnar", "ragnar")
+	assert.Nil(t, err) // Ensure no error during login as normal user
 
 	err = utils.Link(conn, dbname, connStr)
-	assert.NotNil(t, err) // Ensure no error during linking db
+	assert.NotNil(t, err) // Ensure error during linking database with normal user
 }
 
+// TestClean tests the cleanup utility
 func TestClean(t *testing.T) {
 	utils.CleanUp()
 }
